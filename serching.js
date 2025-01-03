15fe78b5e6efc891cedad5aec0d8ced071022c75
Abhilash j <abhiabhilash1456910@gmail.com>
2025-01-03 20:25:42 +0530

    serching commit was set in 2nd time

--- serching.js
+++ serching.js
@@ -1,2 +1,10 @@
-const name="Abhilash"
-console.log(name)
+const a=21;
+const b=21;
+function sum(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+console.log(sum)
+console.log(sub)
